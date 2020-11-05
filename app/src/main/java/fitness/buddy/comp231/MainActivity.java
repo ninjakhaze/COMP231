0b90e463d65a8d156883fedcca8f3875606a9a60
Ralph <tongolralph@yahoo.ca>
2020-11-05 09:26:28 -0500

    commit onBAckPressed in MainActivity

--- app/src/main/java/fitness/buddy/comp231/MainActivity.java
+++ app/src/main/java/fitness/buddy/comp231/MainActivity.java
@@ -1,7 +1,10 @@
 package fitness.buddy.comp231;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -74,5 +77,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
